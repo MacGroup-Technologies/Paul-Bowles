c3ffd06adba347c4ac302c0ddc72a4da9bb0877a
= <=>
2023-08-23 15:21:58 +0100

    :bug: Mobile fixes

--- src/components/ChronologyListItem.vue
+++ src/components/ChronologyListItem.vue
@@ -12,8 +12,8 @@ import { ref } from 'vue';
 <template>
   <div class="flex justify-end mb-8 gap-10 overflow-hidden">
     <a :name="item.year" />
-    <h2 class="font-heading text-6xl font-bold">{{ item.year }}</h2>
-    <div class="w-1/2 h-auto overflow-hidden cursor-pointer" @click="opened = !opened">
+    <h2 class="font-heading text-3xl md:text-6xl font-bold">{{ item.year }}</h2>
+    <div class="w-1/2 h-auto overflow-hidden cursor-pointer text-md" @click="opened = !opened">
       <p v-html="item.text" class="transition-all" :class="{ 'line-clamp-2' : !opened}" />
       <pre class="my-5 text-primary transition-all" v-if="opened">{{ item.highlight }}</pre>
     </div>
