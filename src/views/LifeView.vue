6f6e237abc10bb3570e5c276c27f6998a793c479
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-12-14 21:45:49 +0100

    update gradient in life

--- src/views/LifeView.vue
+++ src/views/LifeView.vue
@@ -7,8 +7,7 @@ useHead({ title: `Paul Bowles' Life` })
   <div class="">
     <div class="h-screen bg-background-light dark:primary dark:bg-background-dark relative flex items-center">
       <div
-        class="h-full md:w-2/3 z-10 bg-gradient-to-r from-background-light dark:from-background-dark md:from-55% to-transparent flex flex-col justify-center items-start"
-      >
+        class="h-full md:w-2/3 z-10 bg-gradient-to-r from-white/80 dark:from-black/80 md:from-55% to-transparent flex flex-col justify-center items-start">
         <div class="p-5 lg:px-16 2xl:px-20">
           <h1 class="text-8xl font-mono uppercase">Life</h1>
           <p class="md:w-2/3 text-primary text-3xl mt-5">
@@ -16,22 +15,18 @@ useHead({ title: `Paul Bowles' Life` })
           </p>
         </div>
         <div class="p-5 lg:px-16 2xl:px-20">
-          <a
-            href="#content"
-            class="w-20 h-20 md:w-32 md:h-32 flex justify-center items-center rounded-full border-[2px] border-[currentColor] dark:border-white-shade hover:opacity-75"
-          >
+          <a href="#content"
+            class="w-20 h-20 md:w-32 md:h-32 flex justify-center items-center rounded-full border-[2px] border-[currentColor] dark:border-white-shade hover:opacity-75">
             <p class="text-5xl md:text-7xl animate-bounce">&DownArrow;</p>
           </a>
         </div>
       </div>
-      <img
-        src="@/assets/imgs/imageimage.png"
-        class="absolute top-0 right-0 w-full h-full object-cover md:object-scale-down md:object-right-top"
-      />
+      <img src="@/assets/imgs/imageimage.png" class="absolute top-0 right-0 w-full h-full object-cover " />
     </div>
     <a name="content" />
     <div class="px-5 py-10 lg:px-16 2xl:px-20 grid place-items-stretch gap-5 grid-cols-1 md:grid-cols-2">
-      <router-link to="/biography" class="block bg-white dark:bg-opacity-10 hover:-translate-y-2 transition-transform p-5 rounded-xl">
+      <router-link to="/biography"
+        class="block bg-white dark:bg-opacity-10 hover:-translate-y-2 transition-transform p-5 rounded-xl">
         <div class="flex justify-center items-center bg-primary-light rounded-xl">
           <img src="@/assets/imgs/Biography-Thumbnail.png" />
         </div>
@@ -40,7 +35,8 @@ useHead({ title: `Paul Bowles' Life` })
           <p class="mt-2">A night with thunder in the sky he packed his bag and left...</p>
         </div>
       </router-link>
-      <router-link to="/chronology" class="block bg-white dark:bg-opacity-10 hover:-translate-y-2 transition-transform p-5 rounded-xl">
+      <router-link to="/chronology"
+        class="block bg-white dark:bg-opacity-10 hover:-translate-y-2 transition-transform p-5 rounded-xl">
         <div class="flex justify-center items-center bg-primary-light rounded-xl">
           <img src="@/assets/imgs/Chronology-Thumbnail.png" />
         </div>
