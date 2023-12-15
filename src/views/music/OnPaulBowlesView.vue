db81025a87ca36f5849782999b8e2158060a3bcc
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-12-15 02:05:23 +0100

    fix ordering

--- src/views/music/OnPaulBowlesView.vue
+++ src/views/music/OnPaulBowlesView.vue
@@ -10,8 +10,8 @@ const music = [
   'Berstein',
   'Paul Bowles, Composer',
   'Virgil Thompson - Twelfth Night',
+  'Rey Rosa - The Return Trip',
   'Lincoln Center Tribute',
-  'Rey Rosa - The Return Trip'
 ]
 </script>
 <template>
@@ -19,28 +19,18 @@ const music = [
     <div class="px-5 py-20 lg:px-16 2xl:px-20 text-xl lg:text-2xl lg:py-20">
       <div class="flex items-center justify-between">
         <h1 class="text-5xl md:text-6xl font-heading uppercase">on paul bowles music</h1>
-        <router-link
-          to="#back"
-          @click.prevent="route.go(-1)"
-          class="hover:opacity-75 hover:-translate-x-5 transition-transform"
-        >
+        <router-link to="#back" @click.prevent="route.go(-1)"
+          class="hover:opacity-75 hover:-translate-x-5 transition-transform">
           <icon-back />
         </router-link>
       </div>
       <div class="my-10">
         <p>A selection of texts on the music of Paul Bowles</p>
         <div
-          class="grid grid-cols-1 md:grid-cols-3 gap-20 2xl:gap-48 justify-between py-10 lg:px-16 2xl:px-20 text-xl lg:text-2xl text-center"
-        >
-          <router-link
-            :to="'/music/on-paul-bowless-music/' + encodeURI(item)"
-            class="flex flex-col items-center gap-3"
-            v-for="(item, index) in music"
-            :key="index"
-          >
-            <div
-              class="bg-primary-light w-80 px-20 py-28 rounded-md flex justify-center items-center"
-            >
+          class="grid grid-cols-1 md:grid-cols-3 gap-20 2xl:gap-48 justify-between py-10 lg:px-16 2xl:px-20 text-xl lg:text-2xl text-center">
+          <router-link :to="'/music/on-paul-bowless-music/' + encodeURI(item)" class="flex flex-col items-center gap-3"
+            v-for="(item, index) in music" :key="index">
+            <div class="bg-primary-light w-80 px-20 py-28 rounded-md flex justify-center items-center">
               <icon-musical />
             </div>
             {{ item }}
