f3d2dfd785ba7e9757d6b66a61c16f889e5e2090
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-01-05 15:18:28 +0100

    --annex

--- src/services/library.ts
+++ src/services/library.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-export function fetchLibrary(page:number = 1) {
-  return axios.get(`${baseURL}/v1/archive/library?page=${page}`).then(response=>{
+export async function fetchLibrary(page:number = 1, query:string = "") {
+  return await axios.get(`${baseURL}/v1/archive/library?page=${page}&query=${query}`).then(response=>{
     const output ={
       data : {
         results: response.data.libraries.results,
