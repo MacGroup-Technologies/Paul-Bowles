6c08c6fa49baa66bdeb13865faa6693a9c31e12a
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-02-06 12:05:27 +0100

    fix issue with writings view pagination

--- src/views/writings/WritingView.vue
+++ src/views/writings/WritingView.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
 import ContributionsListItem from '@/components/ContributionsToPeriodicalsListItem.vue'
 import { useHead } from '@unhead/vue'
 import { onMounted, reactive } from 'vue'
@@ -8,8 +8,69 @@ import { useThemeStore } from '@/stores/theme'
 import { getUrl } from '../../services/misc';
 import { ArrowLeft, ArrowRight } from '@element-plus/icons-vue';
 
-const router = useRoute()
-const route = useRouter()
+const route = useRoute()
+const router = useRouter()
+const pagination = reactive({
+  next: null,
+  previous: null,
+  page: null,
+});
+const PAGE_URL = window.location.pathname;
+const PAGE_QUERY = route.query.query;
+
+function getNextPage() {
+  return PAGE_URL + `?page=${Number(route.query.page || 1) + 1}&query=${route.query.query || ''}`;
+}
+
+function getPreviousPage() {
+  return PAGE_URL + `?page=${Number(route.query.page) - 1 || 1}&query=${route.query.query || ''}`;
+}
+
+const filter = reactive({
+  keyword: route.query.query || '',
+  page: Number(route.query.page) || 1,
+});
+
+function clearFilters() {
+  router.push({ path: PAGE_URL, query: { query: '' } });
+}
+
+async function fetchPageData() {
+  setLoading(true);
+
+  let params;
+  switch (route.params.title) {
+    case 'Travels':
+      params = 'Travel';
+      break;
+
+    default:
+      params = route.params.title;
+      break;
+  }
+  try {
+    const response = await fetchMusicBookAlt(params, filter.page)
+
+    pagination.next = response.data.next;
+    pagination.previous = response.data.previous;
+    writing.item = response.data.results
+
+    setLoading(false)
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  } catch (error: any) {
+    setError(error)
+    console.log(error)
+  }
+}
+
+onBeforeRouteUpdate((to, from) => {
+  if (to !== from) {
+    filter.page = Number(to.query.page) || 1;
+    fetchPageData()
+  }
+})
+
 const writing = reactive({ next: "", previous: "", item: [] as any })
 
 const setLoading = function (val: boolean) {
@@ -20,63 +81,35 @@ const setError = function (val: string) {
   useThemeStore().updateError(val)
 }
 
-const loadUrl = async function (url: string) {
-  setLoading(true)
-  try {
-    const response = await getUrl(url)
-    writing.item = response.data.results
-    writing.next = response.data.next
-    writing.previous = response.data.previous
-    setLoading(false)
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } catch (error: any) {
-    setError(error)
-    console.log(error)
-  }
-}
-
 const fetchWriting = async function () {
-  setLoading(true)
-  let params;
-  switch (router.params.title) {
-    case 'Travels':
-      params = 'Travel';
-      break;
 
-    default:
-      params = router.params.title;
-      break;
-  }
-  try {
-    const response = await fetchMusicBookAlt(params)
-    writing.item = response.data.results
-    writing.next = response.data.next
-    writing.previous = response.data.previous
-    setLoading(false)
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } catch (error: any) {
-    setError(error)
-    console.log(error)
-  }
 }
 
 onMounted(async () => {
-  await fetchWriting()
-})
+  setLoading(true);
+  try {
+    fetchPageData();
+  } catch (error: any) {
+    console.log(error);
+    setError(error);
+  }
+});
 
-useHead({ title: `Paul Bowles ${router.params.title}` })
+
+
+useHead({ title: `Paul Bowles ${route.params.title}` })
 </script>
 <template>
   <div class="">
     <div class="px-5 py-20 lg:px-16 2xl:px-20 text-xl lg:text-2xl lg:py-20">
       <div class="flex items-center justify-between">
-        <h1 class="text-3xl md:text-6xl font-heading uppercase">{{ router.params.title }}</h1>
-        <router-link to="#back" @click.prevent="route.go(-1)"
+        <h1 class="text-3xl md:text-6xl font-heading uppercase">{{ route.params.title }}</h1>
+        <router-link to="#back" @click.prevent="router.go(-1)"
           class="hover:opacity-75 scale-75 md:scale-100 hover:-translate-x-5 transition-transform">
           <icon-back />
         </router-link>
       </div>
-      <div class="mt-16" v-if="router.params.title === 'Short Stories'">
+      <div class="mt-16" v-if="route.params.title === 'Short Stories'">
         <h2 class="uppercase text-4xl">Introduction</h2>
         <p class="mt-5 text-justify">
           Of his first collection of stories, <i>The Delicate Prey (1950)</i>, Paul Bowles wrote:
@@ -88,23 +121,23 @@ useHead({ title: `Paul Bowles ${router.params.title}` })
           protagonists were born.”
         </p>
       </div>
-      <p class="mt-16 text-justify" v-else-if="router.params.title === 'Novels'">
+      <p class="mt-16 text-justify" v-else-if="route.params.title === 'Novels'">
         Paul Bowles wrote three novels set in North Africa: <i>The Sheltering Sky</i>,
         <i>Let it Come Down</i> and <i>The Spider's House</i>. Up Above the World is set in Central
         America, and <i>Too Far from Home</i>, a novella, in Mali.
       </p>
-      <p class="mt-16 text-justify" v-else-if="router.params.title === 'Contributions to Periodicals'">
+      <p class="mt-16 text-justify" v-else-if="route.params.title === 'Contributions to Periodicals'">
         By the time of his death in Tangier, Morocco, in 1999, Paul Bowles had become a legendary
         figure in modern literary culture. The range of his literary work includes poems, short
         stories, novels, travel writing, essays, music criticism, autobiography, and translations.
       </p>
-      <p class="mt-16 text-justify" v-else-if="router.params.title === 'Autobiography'">
+      <p class="mt-16 text-justify" v-else-if="route.params.title === 'Autobiography'">
         In 1972 Bowles published <i>Without Stopping</i>, his book-length autobiography. In this
         section we present two pages of its typescript along with two short autobiographical texts.
         “Paul Bowles, His Life” could be called poetic as opposed to the more factual one written
         for the <i>Contemporary Authors Autobiographical Series</i> (Detroit, 1984).
       </p>
-      <div class="mt-16 text-justify" v-else-if="router.params.title === 'Music Criticism'">
+      <div class="mt-16 text-justify" v-else-if="route.params.title === 'Music Criticism'">
         <p>
           From 1939 through early 1945, the only prose Paul Bowles published was music criticism. He
           served on the music reviewing staff of the New York Herald <i>Tribune</i>, where his friend
@@ -114,7 +147,7 @@ useHead({ title: `Paul Bowles ${router.params.title}` })
         </p>
         <p>Below is a selection of Paul Bowles’s music criticism :</p>
       </div>
-      <div class="mt-10" v-if="router.params.title === 'Music Criticism'">
+      <div class="mt-10" v-if="route.params.title === 'Music Criticism'">
         <ol class="list-decimal mt-10 ml-10">
           <li v-for="item in writing.item" :key="item.id">
             <router-link :to="`music-criticism/${item.title}`" class="block rounded overflow-hidden" :key="item.id">
@@ -124,7 +157,7 @@ useHead({ title: `Paul Bowles ${router.params.title}` })
         </ol>
       </div>
 
-      <div class="mt-10" v-else-if="router.params.title === 'Autobiography'">
+      <div class="mt-10" v-else-if="route.params.title === 'Autobiography'">
         <ol class="list-decimal mt-10 ml-10">
           <li v-for="item in writing.item" :key="item.id">
             <router-link :to="`autobiography/${item.title}`" class="block rounded overflow-hidden" :key="item.id">
@@ -134,12 +167,12 @@ useHead({ title: `Paul Bowles ${router.params.title}` })
         </ol>
       </div>
 
-      <ol class="list-decimal- mt-10 ml-10" v-else-if="router.params.title === 'Contributions to Periodicals'">
-        <li v-for="(item,index) in writing.item" :key="item.id">
-          <contributions-list-item :index="index" :item="item" ></contributions-list-item>
+      <ol class="list-decimal- mt-10 ml-10" v-else-if="route.params.title === 'Contributions to Periodicals'">
+        <li v-for="(item, index) in writing.item" :key="item.id">
+          <contributions-list-item :index="index" :item="item"></contributions-list-item>
         </li>
       </ol>
-      <ol class="list-decimal mt-10 ml-10" v-else-if="router.params.title === 'Poetry'">
+      <ol class="list-decimal mt-10 ml-10" v-else-if="route.params.title === 'Poetry'">
         <li v-for="item in writing.item" :key="item.id">
           <h3 class="mt-4">{{ item.title }}</h3>
         </li>
@@ -150,19 +183,37 @@ useHead({ title: `Paul Bowles ${router.params.title}` })
           <h3 class="mt-4">{{ item.title }}</h3>
         </div>
       </div>
-      <div class="flex justify-center mt-10" v-if="router.params.title !== 'Autobiography'">
+
+      <div class="flex justify-center mt-10" v-if="route.params.title !== 'Autobiography'">
         <el-button-group>
-          <el-button type="primary" class="bg-primary" :icon="ArrowLeft" style="padding: 20px 25px"
-            :disabled="writing.previous === ''" @click="loadUrl(writing.previous)">
-            Previous Page
-          </el-button>
-          <el-button type="primary" class="bg-primary" style="padding: 20px 25px" :disabled="writing.next === ''"
-            @click="loadUrl(writing.next)">
-            Next Page
+
+          <router-link v-if="pagination.previous" :to="getPreviousPage()"
+            class="el-button--primary el-button--large el-button">
+            <el-icon class="el-icon--left">
+              <ArrowLeft />
+            </el-icon>
+            Prev
+          </router-link>
+          <p v-else class="is-disabled el-button--primary el-button--large el-button">
+            <el-icon class="el-icon--left">
+              <ArrowLeft />
+            </el-icon>
+            Prev
+          </p>
+
+          <router-link v-if="pagination.next" :to="getNextPage()" class="el-button--primary el-button--large el-button">
+            Next
             <el-icon class="el-icon--right">
               <ArrowRight />
             </el-icon>
-          </el-button>
+          </router-link>
+          <p v-else class="is-disabled el-button--primary el-button--large el-button">
+            Next
+            <el-icon class="el-icon--right">
+              <ArrowRight />
+            </el-icon>
+          </p>
+
         </el-button-group>
       </div>
     </div>
