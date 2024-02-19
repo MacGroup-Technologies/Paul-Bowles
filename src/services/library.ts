d94ac5f895e2455d7d9925836e7fa0d909ef8f38
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-02-19 08:03:30 +0100

    fix https://macgrouptech.atlassian.net/browse/PB-54

--- src/services/library.ts
+++ src/services/library.ts
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-export async function fetchLibrary(page:number = 1, query:string = "") {
-  return await axios.get(`${baseURL}/v1/archive/library?page=${page}&query=${query}`).then(response=>{
+export async function fetchLibrary(page:number = 1, query:string = "", has_images:boolean) {
+  const url = new URL(`${baseURL}/v1/archive/library`)
+  url.searchParams.set("page", String(page))
+  url.searchParams.set("query", query)
+  if (has_images !== undefined){
+    url.searchParams.set("has_images", String(has_images))
+  }
+
+  return await axios.get(url.toString()).then(response=>{
     const output ={
       data : {
         results: response.data.libraries.results,
