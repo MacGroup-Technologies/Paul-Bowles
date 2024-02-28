ac4008707f7f207a74bb71d429ece46db8f8d5fe
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-02-28 13:48:48 +0100

    fix viography cover title

--- src/views/BiographyView.vue
+++ src/views/BiographyView.vue
@@ -7,12 +7,9 @@ useHead({ title: `Paul Bowles's Biography` })
   <div class="">
     <div class="h-screen bg-background-light dark:primary dark:bg-background-dark relative flex items-center">
       <div
-        class="h-full md:w-2/3 z-10 bg-gradient-to-r from-white/80 dark:from-black/80 md:from-55% to-transparent flex flex-col justify-center items-start">
+        class="h-full md:w-2/3 z-10 bg-gradient-to-r from-white/80 dark:from-black/80 md:from-55% to-white/10 w-full sm:to-transparent flex flex-col justify-center items-start">
         <div class="p-5 lg:px-16 2xl:px-20">
-          <h1 class="text-8xl font-mono uppercase">Biography</h1>
-          <p class="md:w-2/3 text-3xl mt-5">
-            A night with thunder in the sky he packed his bag and left...
-          </p>
+          <h1 class="text-6xl sm:text-8xl font-mono uppercase">Biography</h1>
         </div>
         <div class="p-5 lg:px-16 2xl:px-20">
           <a href="#content"
