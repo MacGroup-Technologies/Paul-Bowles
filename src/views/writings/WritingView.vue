7b979a8fad7ce3a32a59cc07ace43fb70aac5ae3
Johnson John Boluwatife <jaycewap@gmail.com>
2024-04-24 16:15:57 +0100

    feat: HTML codes in writings

--- src/views/writings/WritingView.vue
+++ src/views/writings/WritingView.vue
@@ -188,6 +188,7 @@ useHead({ title: `Paul Bowles ${route.params.title}` })
           </div>
           <img v-else src="@/assets/imgs/writings-thumbnail.png" />
           <h3 class="text-center mt-4">{{ item.title }}</h3>
+          <div v-html="item.html_code" v-if="item.html_code"></div>
         </div>
       </div>
 
